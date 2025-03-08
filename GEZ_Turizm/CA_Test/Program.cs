b31f47ed22bc825f69f78f827625255a4762f1c9
Ahmet <ahmet0caputlu@gmail.com>
2025-03-08 06:45:39 +0300

    Console Uygulamasında WebUser ve WebUserProfile Test edildi sorunsuz çalışıyor.

---
+++ GEZ_Turizm/CA_Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using Data_Access.Context;
+using Data_Access.Entities;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GEZContext context = new GEZContext();
+
+    }
+}
