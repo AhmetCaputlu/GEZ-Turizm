280fa5d902c9d458d3921f5bcaa63d5278f82317
Ahmet <ahmet0caputlu@gmail.com>
2025-03-10 04:57:20 +0300

    .

---
+++ GEZ_Turizm/DAL/Data_Access/Entities/Events/Events.cs
@@ -0,0 +1,9 @@
+﻿using Data_Access.Entities.Abstracts;
+
+namespace Data_Access.Entities.Events
+{
+    public class Events: BaseEntity
+    {
+        //Burada belirli bir alanda verilecek aktivitelerin özellikleri olacak.Konser,Mangal Partisi vs düşün bunun üzerine
+    }
+}
---
+++ GEZ_Turizm/DAL/Data_Access/Entities/Tours/Tour.cs
@@ -0,0 +1,9 @@
+﻿using Data_Access.Entities.Abstracts;
+
+namespace Data_Access.Entities.Tours
+{
+    public class Tour : BaseEntity
+    {
+       //Burada etkinlikten ziyade bir güzergah üzerinde belirli yerlerde mola verilecek belirli yerlerde seyahat edilecek vs
+    }
+}
